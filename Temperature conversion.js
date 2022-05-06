1a9e18726cd58a049f109fbdce2b9097acadc9ea
Arnold Lupamo <arnoldlupamo@gmail.com>
2022-05-06 14:04:15 +0300

    first commit

---
+++ Temperature conversion.js
@@ -0,0 +1,7 @@
+const kelvin = 0; // temperature of the day
+const celsius = kelvin - 273; //1 celsius is -273 kelvin
+
+const fahrenheit = Math.floor(celsius*(9/5)+32); //conversion of celsius to fahrenheit.
+
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit`)
+
